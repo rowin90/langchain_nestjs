9becfacca03e86db0fb4ffb0a4ff038c188a8f10
饶驹 <raoju1580@qq.com>
2024-11-02 22:54:01 +0800

    ✨ feat: 新增优化版的rag问答

--- src/rag/rag.controller.ts
+++ src/rag/rag.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { RagService } from './rag.service';
 
 @Controller('rag')
 export class RagController {
   constructor(private readonly ragService: RagService) {}
+
+  @Post('/prepareTextToFaiss')
+  async prepareTextToFaiss() {
+    return await this.ragService.prepareTextToFaiss();
+  }
+
+  @Post('/normalRetriever')
+  async normalRetriever(@Body('question') question: string) {
+    return await this.ragService.normalRetriever(question);
+  }
+
+  @Post('/highRetriever')
+  async highRetriever(@Body('question') question: string) {
+    return await this.ragService.highRetriever(question);
+  }
 }
--- src/rag/rag.service.ts
+++ src/rag/rag.service.ts
@@ -5,7 +5,10 @@ import * as path from 'path';
 import { FaissStore } from '@langchain/community/vectorstores/faiss';
 import { EmbeddingModel } from '../ai/model/embeddingModel';
 import { Document } from '@langchain/core/documents';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { RunnableSequence } from '@langchain/core/runnables';
+import { GlmModelProvider } from '../ai/model/glm';
+import { StringOutputParser } from '@langchain/core/output_parsers';
 
 @Injectable()
 export class RagService {
@@ -41,6 +44,7 @@ export class RagService {
     console.log('准备存入 vectorStore');
     await vectorStore.save(this.embeddings_directory);
     console.log('完成');
+    return 'ok';
   }
 
   async loadFaiss() {
@@ -56,7 +60,11 @@ export class RagService {
     return retriever;
   }
 
-  async normalRetriever() {
+  /**
+   * 普通的检索回答
+   * @param question
+   */
+  async normalRetriever(question: string) {
     const retriever = await this.loadFaiss();
 
     const convertDocsToString = (documents: Document[]): string => {
@@ -70,8 +78,55 @@ export class RagService {
     ]);
 
     const result = await contextRetrieverChain.invoke({
-      question: '什么是基础设施',
+      question,
     });
-    console.log('result :', result);
+    return result;
+  }
+
+  /**
+   * 优化检索回答
+   * @param question
+   */
+  async highRetriever(question: string) {
+    const retriever = await this.loadFaiss();
+
+    const convertDocsToString = (documents: Document[]): string => {
+      return documents.map((document) => document.pageContent).join('\n');
+    };
+
+    const contextRetrieverChain = RunnableSequence.from([
+      (input) => input.question,
+      retriever,
+      convertDocsToString,
+    ]);
+
+    const TEMPLATE = `
+你是一个本次事故的分析者，根据文章详细解释和回答问题，你在回答时会引用作品原文。
+并且回答时仅根据原文，尽可能回答用户问题，如果原文中没有相关内容，你可以回答“原文中没有相关内容”，
+
+以下是原文中跟用户回答相关的内容：
+{context}
+
+现在，你需要基于原文，回答以下问题：
+{question}`;
+
+    const prompt = ChatPromptTemplate.fromTemplate(TEMPLATE);
+
+    const model = new GlmModelProvider().createModel();
+
+    const ragChain = RunnableSequence.from([
+      {
+        context: contextRetrieverChain,
+        question: (input) => input.question,
+      },
+      prompt,
+      model,
+      new StringOutputParser(),
+    ]);
+
+    const answer = await ragChain.invoke({
+      question,
+    });
+    return answer;
   }
 }
