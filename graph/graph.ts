f88bd12c31e7bc8d796c25a7e8917ac58732c0b2
饶驹 <raoju1580@qq.com>
2025-03-19 14:17:58 +0800

    feat(graph): 编译并调用状态图
    - 在 graph.ts 中编译了一个简单图，并添加了调用示例
    - 在 state.ts 中添加了 userName 字段，用于存储用户名称

--- graph/graph.ts
+++ graph/graph.ts
@@ -4,60 +4,12 @@
  */
 import { StateGraph, START } from '@langchain/langgraph';
 import { RunnableConfig } from '@langchain/core/runnables';
-import { StateAnnotation } from './state.js';
+import { StateAnnotation } from './state';
 
-/**
- * Define a node, these do the work of the graph and should have most of the logic.
- * Must return a subset of the properties set in StateAnnotation.
- * @param state The current state of the graph.
- * @param config Extra parameters passed into the state graph.
- * @returns Some subset of parameters of the graph state, used to update the state
- * for the edges and nodes executed next.
- */
 const callModel = async (
   state: typeof StateAnnotation.State,
   _config: RunnableConfig,
 ): Promise<typeof StateAnnotation.Update> => {
-  /**
-   * Do some work... (e.g. call an LLM)
-   * For example, with LangChain you could do something like:
-   *
-   * ```bash
-   * $ npm i @langchain/anthropic
-   * ```
-   *
-   * ```ts
-   * import { ChatAnthropic } from "@langchain/anthropic";
-   * const model = new ChatAnthropic({
-   *   model: "claude-3-5-sonnet-20240620",
-   *   apiKey: process.env.ANTHROPIC_API_KEY,
-   * });
-   * const res = await model.invoke(state.messages);
-   * ```
-   *
-   * Or, with an SDK directly:
-   *
-   * ```bash
-   * $ npm i openai
-   * ```
-   *
-   * ```ts
-   * import OpenAI from "openai";
-   * import { call } from '../../node_modules/@langchain/langgraph/dist/pregel/call';
-   const openai = new OpenAI({
-   *   apiKey: process.env.OPENAI_API_KEY,
-   * });
-   *
-   * const chatCompletion = await openai.chat.completions.create({
-   *   messages: [{
-   *     role: state.messages[0]._getType(),
-   *     content: state.messages[0].content,
-   *   }],
-   *   model: "gpt-4o-mini",
-   * });
-   * ```
-   */
-  console.log('Current state:', state);
   return {
     messages: [
       {
@@ -65,6 +17,7 @@ const callModel = async (
         content: `Hi there! How are you?`,
       },
     ],
+    userName: 'youyou',
   };
 };
 
@@ -79,13 +32,6 @@ const customAdd = () => {
   };
 };
 
-/**
- * Routing function: Determines whether to continue research or end the builder.
- * This function decides if the gathered information is satisfactory or if more research is needed.
- *
- * @param state - The current state of the research builder
- * @returns Either "callModel" to continue research or END to finish the builder
- */
 export const route = (
   state: typeof StateAnnotation.State,
 ): '__end__' | 'callModel' => {
@@ -115,5 +61,15 @@ const builder = new StateGraph(StateAnnotation)
   .addConditionalEdges('customAdd', route);
 
 export const graph = builder.compile();
+
+// 调用 graph
+(async function () {
+  const res = await graph.invoke({ messages: [], userName: 'haha' });
+
+  console.log('=>(demo.ts 26) username', res.userName);
+  res.messages.forEach((m) => {
+    console.log('=>(graph.ts 72) m', m.content);
+  });
+})();
 
 graph.name = 'New Agent';
