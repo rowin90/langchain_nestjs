29715927273a26c434bf6eceb76db664a45eabbb
饶驹 <raoju1580@qq.com>
2025-03-26 15:56:20 +0800

    actor(graphref): 注释掉多Agent功能的实现
    - 注释掉了8.mutil_graph.ts文件中的多Agent功能实现代码
    - 在study.huggingface.service.ts中添加了verctor方法，用于测试向量搜索功能

--- src/study_rag/study.huggingface.service.ts
+++ src/study_rag/study.huggingface.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { pipeline } from '@huggingface/transformers';
 import { FaissStore } from '@langchain/community/vectorstores/faiss';
 import { OllamaEmbeddings } from '@langchain/community/embeddings/ollama';
+import { Document } from '@langchain/core/documents';
 
 @Injectable()
 export class StudyHuggingFaceService {
@@ -77,5 +78,77 @@ export class StudyHuggingFaceService {
     const db = await FaissStore.load('./faiss_index', this.embeddingsModel);
     const res = await db.similaritySearchWithScore('我喜欢篮球', 2);
     console.log('=>(study.huggingface.service.ts 46) res', res);
+  }
+
+  async verctor() {
+    const documents = [
+      new Document({
+        pageContent: '笨笨是一只很喜欢睡觉的猫咪',
+        metadata: { page: 1 },
+      }),
+      new Document({
+        pageContent: '我喜欢在夜晚听音乐，这让我感到放松。',
+        metadata: { page: 2 },
+      }),
+      new Document({
+        pageContent: '猫咪在窗台上打盹，看起来非常可爱',
+        metadata: { page: 3 },
+      }),
+      new Document({
+        pageContent: '学习新技能是每个人都应该追求的目标。',
+        metadata: { page: 4 },
+      }),
+      new Document({
+        pageContent: '我最喜欢的食物是意大利面，尤其是番茄酱的那种。',
+        metadata: { page: 5 },
+      }),
+      new Document({
+        pageContent: '昨晚我做了一个奇怪的梦，梦见自己在太空飞行。',
+        metadata: { page: 6 },
+      }),
+      new Document({
+        pageContent: '我的手机突然关机了，让我有些焦虑。',
+        metadata: { page: 7 },
+      }),
+      new Document({
+        pageContent: '阅读是我每天都会做的事情，我觉得很充实。',
+        metadata: { page: 8 },
+      }),
+      new Document({
+        pageContent: '他们一起计划了一次周末的野餐，希望天气能好。',
+        metadata: { page: 9 },
+      }),
+      new Document({
+        pageContent: '我的狗喜欢追逐球，看起来非常开心。',
+        metadata: { page: 10 },
+      }),
+    ];
+
+    // 先存储向量，后续可以直接加载即可，节约时间
+    // const db = await FaissStore.fromDocuments(documents, this.embeddingsModel);
+    // db.save('./faiss_index');
+    const db = await FaissStore.load('./faiss_index', this.embeddingsModel);
+    const res = await db.similaritySearchWithScore('我喜欢阅读', 5);
+    // const r = res.filter(([doc, score]) => score < 15000);
+    // const res = await db.similaritySearch('我养了一只猫，叫笨笨');
+    /**
+     *    [
+     *     { pageContent: '阅读是我每天都会做的事情，我觉得很充实。', metadata: [Object] },
+     *     14143.166015625
+     *   ],
+     *   [
+     *     { pageContent: '学习新技能是每个人都应该追求的目标。', metadata: [Object] },
+     *     15260.66796875
+     *   ],
+     *   [
+     *     { pageContent: '他们一起计划了一次周末的野餐，希望天气能好。', metadata: [Object] },
+     *     16685.11328125
+     *   ],
+     *   [
+     *     { pageContent: '我的狗喜欢追逐球，看起来非常开心。', metadata: [Object] },
+     *     16977.6640625
+     *   ],
+     */
+    console.log('=>(study.huggingface.service.ts 129) res', res);
   }
 }
